f7f3c41a065b94a52e2cb4713a1f2d14414ad379
Will Barton <william.barton@cfpb.gov>
2017-05-09 09:08:52 -0400

    Use pandoc to convert README.md to rst

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from setuptools import setup, find_packages
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    long_description = open('README.md', 'r').read()
+
 
 setup(
     name='mkDOCter',
-    version='1.0.1',
+    version='1.0.3',
     url='https://github.com/willbarton/mkDOCter',
     license='CC0',
     description='mkdocs theme based on DOCter',
+    long_description=long_description,
     author='Will Barton',
     author_email='william.barton@cfpb.gov',
     packages=find_packages(),
